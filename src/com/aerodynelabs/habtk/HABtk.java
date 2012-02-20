5821cfbcc36d18a897bbf4a3c878bac18bb40516
Ethan Harstad <ethanharstad@gmail.com>
2012-02-19 22:55:12 -0600

    Configuring repository and added default class.

---
+++ src/com/aerodynelabs/habtk/HABtk.java
@@ -0,0 +1,17 @@
+package com.aerodynelabs.habtk;
+
+/**
+ * The main class of HABtk
+ * @author eharstad
+ * 
+ */
+public class HABtk {
+
+	/**
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		// Does nothing yet
+	}
+
+}
