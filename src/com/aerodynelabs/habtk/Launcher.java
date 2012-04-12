f2a0078b9476eedcd75097a6111975eced74a1c5
Ethan Harstad <ethanharstad@gmail.com>
2012-04-11 21:47:40 -0500

    Adding preliminary support for launcher with automatic updates.

---
+++ src/com/aerodynelabs/habtk/Launcher.java
@@ -0,0 +1,9 @@
+package com.aerodynelabs.habtk;
+
+public class Launcher {
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
---
+++ src/com/aerodynelabs/habtk/Updater.java
@@ -0,0 +1,23 @@
+package com.aerodynelabs.habtk;
+
+import javax.swing.JFrame;
+
+public class Updater {
+	
+	public Updater() {
+		JFrame frame = new JFrame("Check for Updates");
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+	
+	public static void applyUpdates() {
+		
+	}
+	
+	public static void getUpdates() {
+		
+	}
+
+}
