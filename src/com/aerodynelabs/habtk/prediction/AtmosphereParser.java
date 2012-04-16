1ef17829ec7160b01d3e09753afa562ca6d65f7c
Ethan Harstad <ethanharstad@gmail.com>
2012-04-16 18:14:11 -0500

    Cleaning up after atmosphere move.

--- src/com/aerodynelabs/habtk/prediction/AtmosphereParser.java
+++
@@ -1,9 +0,0 @@
-package com.aerodynelabs.habtk.prediction;
-
-import java.io.File;
-
-public interface AtmosphereParser {
-	
-	public AtmosphereProfile parseAtmosphere(File  file);
-
-}
--- src/com/aerodynelabs/habtk/prediction/AtmosphereSource.java
+++
@@ -1,9 +0,0 @@
-package com.aerodynelabs.habtk.prediction;
-
-import java.io.File;
-
-public interface AtmosphereSource {
-	
-	public File getAtmosphere(int time, double lat, double lon);
-
-}
