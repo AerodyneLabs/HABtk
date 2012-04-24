aad5d85e65a080dfbff1768606b8b305d555a658
Ethan Harstad <ethanharstad@gmail.com>
2012-04-23 20:31:29 -0500

    Placeholders for elevation and navigation services.

---
+++ src/com/aerodynelabs/habtk/prediction/ElevationService.java
@@ -0,0 +1,7 @@
+package com.aerodynelabs.habtk.prediction;
+//TODO Elevation Service
+
+// http://developer.mapquest.com/web/products/open/elevation-service
+public class ElevationService {
+
+}
---
+++ src/com/aerodynelabs/map/NavigationService.java
@@ -0,0 +1,6 @@
+package com.aerodynelabs.map;
+
+// http://developer.mapquest.com/web/products/open/directions-service
+public class NavigationService {
+
+}
